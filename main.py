3b80b90c2c53e8e2532af80d0b3306797066d78b
Thomas Lee <tcl171@gmail.com>
2021-05-30 02:59:32 -0700

    now supports get and update

--- main.py
+++ main.py
@@ -1,41 +1,35 @@
-""" 
-Setup code taken from example code at https://github.com/smartsheet-samples/python-read-write-sheet.
-"""
-# Install the smartsheet sdk with the command: pip install smartsheet-python-sdk
-import smartsheet
-import logging
-import os
+from utils import *
 import sys
+import json
 
-API_TOKEN = os.environ['SMARTSHEET_ACCESS_TOKEN']
-smart = smartsheet.Smartsheet(API_TOKEN)
-smart.errors_as_exceptions(True)
-
-sheetId = 1600831905654660
-sheet = smart.Sheets.get_sheet(sheetId)
-
-columnMap = {}
-for column in sheet.columns:
-    columnMap[column.title] = column.id
-
-
-#Gets the cell of the inputted column name from a row.
-def getRowCell(row, columnName):
-    columnId = columnMap[columnName]
-    return row.get_column(columnId)
-
-#Updates the cell value of the inputted column name from a given row.
-def updateCellValue(row, columnName, updatedValue):
-    cell = getRowCell(row, columnName)
-    cell.value = updatedValue;
-    updatedRow = smart.models.Row()
-    updatedRow.id = row.id
-    updatedRow.cells.append(cell)
-    smart.Sheets.update_rows(sheetId, [updatedRow])
+# s: String in the form {a:b,c:d,e:f} <-- no spaces between elements!
+def stringToDict(s):
+    # Removes the curly braces and splits the different key-value pairs
+    lst = s.replace("{","").replace("}", "").split(",")
+    keyValueLst = [item.split(":") for item in lst]
+    retDict = {}
+    for pair in keyValueLst:
+        retDict[pair[0]] = pair[1]
+    return retDict
 
 """
-if __name__ == "__main__":
+Currently supported operations:
+get PROPS
+update PROPS;UPDATEDPROPS
+"""
+
+
+if __name__ == '__main__':
     args = sys.argv
-    if (len(args) != 2):
-        print('Invalid args. Please input a single string in the form [[org=xyz company; phase=nda; task=execute NDA; in progress; 25%;awaiting response from collaborator]]')
-"""
+    if len(args) != 3:
+        print('Invalid args.')    
+    elif args[1] == 'get':
+        props = stringToDict(args[2])
+        print(getRowValues(props))
+    elif args[1] == 'update':
+        splitArgs = args[2].split(';')
+        props = stringToDict(splitArgs[0])
+        updatedProps = stringToDict(splitArgs[1])
+        rowToUpdate = getRow(props)
+        updateRowValues(rowToUpdate, updatedProps)
+
