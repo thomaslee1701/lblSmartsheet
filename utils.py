af0aeae699fc66e4fdafc66c2efbd9d8721be1f9
Thomas Lee <tcl171@gmail.com>
2021-05-30 13:58:53 -0700

    Moved some logic out into main. Should probably change so that it is not like that.

--- utils.py
+++ utils.py
@@ -1,7 +1,3 @@
-""" 
-Setup code taken from example code at https://github.com/smartsheet-samples/python-read-write-sheet.
-"""
-# Install the smartsheet sdk with the command: pip install smartsheet-python-sdk
 import smartsheet
 import os
 
@@ -9,31 +5,26 @@ API_TOKEN = os.environ['SMARTSHEET_ACCESS_TOKEN']
 smart = smartsheet.Smartsheet(API_TOKEN)
 smart.errors_as_exceptions(True)
 
-SHEET_ID = 1600831905654660
-sheet = smart.Sheets.get_sheet(SHEET_ID)
-
-# Sets up easy access to columns using column names rather than ids
-columnMap = {}
-for column in sheet.columns:
-    columnMap[column.title] = column.id
-
 # row: row object
 # columnName: String
+# columnMap: dictionary of columnName:columnId
 # Gets the cell of the inputted column name from a row.
-def getRowCell(row, columnName):
+def getRowCell(row, columnName, columnMap):
     columnId = columnMap[columnName]
     return row.get_column(columnId)
 
 # props: dictionary (String:String)
+# sheet: sheet object
+# columnMap: dictionary of columnName:columnId
 # Gets the row from the inputted sheet with the inputted properties. They all have to match exactly.
 # Currently using display_value for comparisons. Might switch to value.
 # Returns the row if found. Returns False if not found.
-def getRow(props):
+def getRow(props, sheet, columnMap):
     rows = sheet.rows
     for row in rows:
         found = True
         for key in props.keys():
-            if getRowCell(row, key).display_value != props[key]:
+            if getRowCell(row, key, columnMap).display_value != props[key]:
                 found = False
                 break
         if found:
@@ -42,28 +33,30 @@ def getRow(props):
 
 # props: dictionary (String:String)
 # Returns a list of values in the order of the row.
-def getRowValues(props):
-    row = getRow(props)
+def getRowValues(props, sheet, columnMap):
+    row = getRow(props, sheet, columnMap)
     return [cell.display_value for cell in row.cells]
 
 # HELPER FUNCTION for updateRowValues
 # row: row object
 # columnName: String
 # updatedValue: String
+# columnMap: dictionary of columnName:columnId
 # Updates the cell value of the inputted column name from a given row.
-def updateCellValue(row, columnName, updatedValue):
-    cell = getRowCell(row, columnName)
-    cell.value = updatedValue;
+def updateCellValue(row, columnName, updatedValue, sheet, columnMap):
+    cell = getRowCell(row, columnName, columnMap)
+    cell.value = updatedValue
+    cell.strict = False
     updatedRow = smart.models.Row()
     updatedRow.id = row.id
     updatedRow.cells.append(cell)
-    smart.Sheets.update_rows(SHEET_ID, [updatedRow])
+    smart.Sheets.update_rows(sheet.id, [updatedRow])
 
 # row: row object
 # updatedProps: dictionary (String:String)
 # Updates the values of a given row with the inputted properties from the updatedProps
-def updateRowValues(row, updatedProps):
+def updateRowValues(row, updatedProps, sheet, columnMap):
     for key in updatedProps.keys():
-        updateCellValue(row, key, updatedProps[key])
+        updateCellValue(row, key, updatedProps[key], sheet, columnMap)
 
 #updateRowValues(getRow({"Name":"cheese"}), {"Name":"Josh", "Age":"10", "Salary":"100000"})
